deb5610b993df2cc7c946b2d618bb45eaaff7951
Bir Bireshwar Bala <birbireshwarb@gmail.com>
2025-11-20 01:56:33 +0530

    basic recursion TC: O(m+n) & SC: O(m+n)

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,68 @@
+public class MergeTwoSortedLists {
+
+    // Definition for singly-linked list.
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head;
+
+        if (list1.val <= list2.val) {
+            head = list1;
+            head.next = mergeTwoLists(list1.next, list2);
+        } else {
+            head = list2;
+            head.next = mergeTwoLists(list1, list2.next);
+        }
+
+        return head;
+    }
+
+    // Utility to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    // Main method to test the merge
+    public static void main(String[] args) {
+        MergeTwoSortedLists sol = new MergeTwoSortedLists();
+
+        // List 1: 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        // List 2: 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        System.out.println("List 1:");
+        printList(l1);
+        System.out.println("List 2:");
+        printList(l2);
+
+        ListNode merged = sol.mergeTwoLists(l1, l2);
+
+        System.out.println("Merged List:");
+        printList(merged);
+    }
+}
